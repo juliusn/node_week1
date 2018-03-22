33f9d3290f1c8276b20336569094ca676639fc65
Julius Niiniranta <julius.niiniranta@quattrofolia.com>
2018-03-22 18:32:55 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,116 @@
+let picArray;
+let map;
+const thumbsList = document.querySelector('.thumbsList');
+const request = new Request('pictures.json');
+const select = document.querySelector('.sort');
+
+fetch(request).then((response) => {
+  return response.json();
+}).then((json) => {
+  picArray = json;
+
+  select.removeAttribute('disabled');
+  const opt1 = document.createElement('option');
+  opt1.value = 'title';
+  opt1.text = 'Title';
+
+  const opt2 = document.createElement('option');
+  opt2.value = 'category';
+  opt2.text = 'Category';
+
+  const opt3 = document.createElement('option');
+  opt3.value = 'date';
+  opt3.text = 'Date';
+
+  select.add(opt1);
+  select.add(opt2);
+  select.add(opt3);
+  select.setAttribute('onchange', 'sort()');
+
+  update();
+});
+
+class Modal {
+  constructor(overlay, pic) {
+    this.overlay = overlay;
+    const closeButton = overlay.querySelector('.button-close');
+    const img = overlay.querySelector('.image-display');
+    const location = {lat: pic.coordinates.lat, lng: pic.coordinates.lng};
+    map = new google.maps.Map(document.querySelector('.map-container'), {
+      zoom: 4,
+      center: location,
+      disableDefaultUI: true,
+    });
+    const marker = new google.maps.Marker({
+      position: location,
+      map: map,
+    });
+
+    console.log(pic);
+    img.src = pic.original;
+    closeButton.addEventListener('click', this.close.bind(this));
+    overlay.addEventListener('click', e => {
+      if (e.srcElement.id === this.overlay.id) {
+        this.close();
+      }
+    });
+  }
+
+  open() {
+    this.overlay.classList.remove('is-hidden');
+  }
+
+  close() {
+    this.overlay.classList.add('is-hidden');
+  }
+}
+
+function initMap() {
+  const uluru = {lat: -25.363, lng: 131.044};
+  map = new google.maps.Map(document.querySelector('.map-container'), {
+    zoom: 4,
+    center: uluru,
+    disableDefaultUI: true,
+  });
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+function sort() {
+  const selected = select.options[select.selectedIndex].value;
+  picArray.sort((a, b) => a[selected] > b[selected]);
+  update();
+}
+
+function update() {
+  thumbsList.innerHTML='';
+  picArray.map((pic) => {
+    const img = document.createElement('img');
+    img.src = pic.thumbnail;
+    img.addEventListener('click', (e) => {
+      const modal = new Modal(document.querySelector('.modal-overlay'), pic);
+      window.openModal = modal.open.bind(modal);
+      window.openModal();
+    });
+
+    const title = document.createElement('li');
+    title.appendChild(document.createTextNode('Title: ' + pic.title));
+    const details = document.createElement('li');
+    details.appendChild(document.createTextNode('Details: ' + pic.details));
+    const date = document.createElement('li');
+    const dateReadable = new Date(pic.time.replace(' ', 'T')).toDateString();
+    date.appendChild(document.createTextNode('Date: ' + dateReadable));
+
+    const thumbsListItem = document.createElement('li');
+    const thumbInfo = document.createElement('ul');
+    thumbInfo.appendChild(title);
+    thumbInfo.appendChild(details);
+    thumbInfo.appendChild(date);
+
+    thumbsListItem.appendChild(img);
+    thumbsListItem.appendChild(thumbInfo);
+    thumbsList.appendChild(thumbsListItem);
+  });
+}
